d124725b6261edf4dc5c05509d97a004213965f6
lizz <lizz@pku.edu.cn>
2015-08-05 18:18:34 +0800

    add ef6.

---
+++ Imagenet/Model/ImgnetContext.cs
@@ -0,0 +1,56 @@
+namespace Imagenet.Model
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity;
+    using System.Linq;
+
+    public class ImgnetContext : DbContext
+    {
+        // Your context has been configured to use a 'ImgnetContext' connection string from your application's 
+        // configuration file (App.config or Web.config). By default, this connection string targets the 
+        // 'Imagenet.Model.ImgnetContext' database on your LocalDb instance. 
+        // 
+        // If you wish to target a different database and/or database provider, modify the 'ImgnetContext' 
+        // connection string in the application configuration file.
+        public ImgnetContext()
+            : base("name=ImgnetContext")
+        {
+        }
+
+        // Add a DbSet for each entity type that you want to include in your model. For more information 
+        // on configuring and using a Code First model, see http://go.microsoft.com/fwlink/?LinkId=390109.
+
+        public virtual DbSet<Synset> Synsets { get; set; }
+        public virtual DbSet<Image> Images { get; set; }
+    }
+
+    public class Synset
+    {
+        [Key, MaxLength(16)]
+        public string Wnid { get; set; }
+        public string Words { get; set; }
+        public string Glosses { get; set; }
+        public bool? IsAvailable { get; set; }
+
+        public string ParentId { get; set; }
+        [ForeignKey("ParentId")]
+        public virtual Synset Parent { get; set; }
+        [InverseProperty("Synset")]
+        public List<Image> Images { get; set; }
+    }
+
+    public class Image
+    {
+        public int ImageId { get; set; }
+        [Index, MaxLength(16)]
+        public string Wnid { get; set; }
+        public int? Index { get; set; }
+        public string Url { get; set; }
+
+        [ForeignKey("Wnid")]
+        public virtual Synset Synset { get; set; }
+    }
+}
